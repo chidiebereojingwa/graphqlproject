01e66a5bc278d2d1fa9b9122a8df724785f74d57
chidiebereojingwa <ojingwaoji@gmail.com>
2022-06-19 03:25:55 +0100

    Added mongodb

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 
+
+mongoose.connect(
+  "mongodb+srv://somto:Orji5644@cluster0.6y5afyt.mongodb.net/?retryWrites=true&w=majority"
+);
+mongoose.connection.once('open', ()=>{
+  console.log('connected to database')
+})
 // bind express with graphql
 app.use(
   "/graphql",
