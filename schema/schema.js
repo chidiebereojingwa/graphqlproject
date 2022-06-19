826a348a1395b95efe0c1e127df13d608dd31b9b
chidiebereojingwa <ojingwaoji@gmail.com>
2022-06-19 02:21:27 +0100

    Add authorID

--- schema/schema.js
+++ schema/schema.js
@@ -11,9 +11,9 @@ const {
 
 // dummy data
 var books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" },
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
 ];
 
 var authors = [
