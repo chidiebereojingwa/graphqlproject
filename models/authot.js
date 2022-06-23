70428f3f9b34cceee7f8454935d4c54f41cb06c0
chidiebereojingwa <ojingwaoji@gmail.com>
2022-06-23 18:08:25 +0100

    Update autho.js and schema.js

--- models/authot.js
+++ models/authot.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const authorSchema = new Schema({
   name: String,
   age: Number,
+  surmame: String,
 });
 
 module.exports = mongoose.model("Author", authorSchema);
